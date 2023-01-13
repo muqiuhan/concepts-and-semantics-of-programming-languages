ed51a770280039324ad10a53056c1151f0aff6e9
Muqiu Han <muqiu-han@outlook.com>
2023-01-14 07:54:46 +0800

    Saturday, January 14, 2023 7:54 AM

--- CaSoPL/Library.fs
+++ CaSoPL/Library.fs
@@ -2,22 +2,56 @@
 
 module Lang1 =
     type X = string
+    and R = int
     and V = int
-    and Binding = X * V
-    and Environment = List<Binding>
-    and Domain = List<X>
 
-    let rec Env: Environment -> X -> Option<V> =
+    and EnvironmentBinding = X * R
+    and Environment = EnvironmentBinding list
+    and EnvironmentDomain = X list
+
+    and MemoryBinding = R * V
+    and Memory = MemoryBinding list
+    and MemoryDomain = R list
+
+    /// Env(x)
+    let rec Env: Environment -> X -> R option =
         fun env x ->
             match env with
             | [] -> None
-            | (x1, v1) :: t -> if x = x1 then Some v1 else Env t x
+            | (x1, r1) :: t -> if x = x1 then Some r1 else Env t x
 
-    let rec Dom: Environment -> Option<Domain> =
+    /// EnvDom(Env_n)
+    let rec EnvDom: Environment -> EnvironmentDomain option =
         fun env ->
             match env with
             | [] -> None
-            | (x, _) :: t -> Option.bind (fun dom -> Some(x :: dom)) (Dom t)
+            | (x, _) :: t -> Option.bind (fun dom -> Some(x :: dom)) (EnvDom t)
 
-    let AddBinding: Environment -> Binding -> Environment =
+    /// (x, r) ⊕ Env_n
+    let AddEnvBinding: Environment -> EnvironmentBinding -> Environment =
         fun env binding -> binding :: env
+
+    /// Mem(r)
+    let rec Mem: Memory -> R -> V option =
+        fun mem r ->
+            match mem with
+            | [] -> None
+            | (r1, v1) :: t -> if r = r1 then Some v1 else Mem t r
+
+    /// MemDom(Mem_n)
+    let rec MemDom: Memory -> MemoryDomain option =
+        fun mem ->
+            match mem with
+            | [] -> None
+            | (r, _) :: t -> Option.bind (fun dom -> Some(r :: dom)) (MemDom t)
+
+    /// Mem[r := v]
+    let rec AddMemBinding: Memory -> MemoryBinding -> Memory =
+        fun mem (r, v) ->
+            match mem with
+            | [] -> [ (r, v) ]
+            | (r1, v1) :: t ->
+                if r = r1 then
+                    (r1, v) :: t
+                else
+                    (r1, v1) :: (AddMemBinding t (r, v))
